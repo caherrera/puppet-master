db8122e519b3aa093bdb170c4cf138937901a329
Carlos Herrera <carlos.herrera@viajesfalabella.com>
2019-08-30 19:10:27 -0400

    configure puppet agent

--- manifests/config/puppet.pp
+++ manifests/config/puppet.pp
@@ -1,20 +1,24 @@
 class master::config::puppet (
-  Hash $main   = {},
-  Hash $master = {},
+  Boolean $agent = false,
+  Hash $main     = {},
+  Hash $master   = {},
 ) {
 
-  $default_master_section = {
-    'vardir'  => '/opt/puppetlabs/server/data/puppetserver',
-    'logdir'  => '/var/log/puppetlabs/puppetserver',
-    'rundir'  => '/var/run/puppetlabs/puppetserver',
-    'pidfile' => '/var/run/puppetlabs/puppetserver/puppetserver.pid',
-    'codedir' => '/etc/puppetlabs/code',
+  if (!$agent) {
+    $default_master_section = {
+      'vardir'  => '/opt/puppetlabs/server/data/puppetserver',
+      'logdir'  => '/var/log/puppetlabs/puppetserver',
+      'rundir'  => '/var/run/puppetlabs/puppetserver',
+      'pidfile' => '/var/run/puppetlabs/puppetserver/puppetserver.pid',
+      'codedir' => '/etc/puppetlabs/code',
 
 
+    }
+    $_master = $default_master_section + $master
   }
+
   $default_main_section = {}
 
-  $_master = $default_master_section + $master
   $_main = $default_main_section + $main
 
   file { $master::params::puppetconf:
