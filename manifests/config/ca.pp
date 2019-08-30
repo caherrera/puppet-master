60ec2a58bf1cac2b21381fb92252fa15fe54505a
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2019-08-30 13:29:40 -0400

    fix puppetserver ca setup execution

--- manifests/config/ca.pp
+++ manifests/config/ca.pp
@@ -1,7 +1,12 @@
 class master::config::ca () {
+
+  $ca_folder = '/etc/puppetlabs/puppet/ssl'
+
   exec { 'puppetserver-ca-setup':
+
     path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/opt/puppetlabs/bin',
-    command => 'puppetserver ca setup',
+    command => "rm -rf * && /opt/puppetlabs/bin/puppetserver ca setup",
+    cwd     => $ca_folder
 
   }
 
