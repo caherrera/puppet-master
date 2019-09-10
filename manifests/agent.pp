29a0c8b0eed60d0bdeb33503deb27d259de07931
Carlos Herrera <carlos.herrera@viajesfalabella.com>
2019-09-10 12:14:27 -0300

    new arguments to puppet::agent

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -5,17 +5,31 @@
 # @example
 #   include master::agent
 class master::agent (
-
+  Boolean $agent = false,
+  Hash $main     = {},
+  Hash $master   = {},
 ) inherits master::params {
 
   class { master::config::puppet:
-    agent => true
-  }
+    agent  => true,
+    master => $master,
+    main   => $main,
+
+
+  } ~>
 
   class { master::service:
-    service_name => 'puppet'
+
+    service_name    => 'puppet',
+    service_restart => $master::params::service_restart,
+    service_ensure  => $master::params::service_ensure,
+    service_enable  => $master::params::service_enable,
+    service_flags   => $master::params::service_flags,
+    service_manage  => $master::params::service_manage,
+
+
   }
 
-  Class['master::config::puppet'] ~> Class['master::service']
+  
 
 }
